a5289680a6848eee521aea94dee0417867ac3941
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2019-04-12 01:19:53 -0700

    Basic functionality for the Better Event Emitter.

---
+++ index.ts
@@ -0,0 +1,116 @@
+/** A function which takes an argument if it isn't undefined. */
+type OneArgFn<T> = T extends void
+    ? () => void
+    : (arg: T) => void
+
+export interface Listener<T = void> {
+    readonly next: Promise<T>
+    readonly all: AsyncIterableIterator<T>
+    readonly count: number
+    once(fn: OneArgFn<T>): void
+    on(fn: OneArgFn<T>): void
+    every(fn: OneArgFn<T>, errFn: OneArgFn<Error>): void
+}
+
+export interface Broadcaster<T> {
+    activate(...arg: Parameters<OneArgFn<T>>): void
+    deactivate(err: Error): void
+    cancel(): void
+}
+
+// TODO: Cancel should not create any more promises??
+export default class Emitter<T = void> implements Listener<T>, Broadcaster<T> {
+
+    private static readonly CANCEL_KEY = Symbol('Gracefully ending event')
+
+    constructor() { this.makePromise() }
+
+    private reject!: Function
+
+    private resolve!: Function
+
+    private promises: Promise<T>[] = []
+
+    /**
+     * Resolves when event is activated.
+     * Rejects when event is deactivated or cancelled.
+     */
+    get next() { return this.promises[this.count] }
+
+    /** Async generator which listens to all events. */
+    get all() { return this.promiseGenerator() }
+
+    /** The number of times this event has been activated or deactivated. */
+    get count() { return this.promises.length - 1 }
+
+    /** Triggers an event. */
+    public activate(...arg: Parameters<OneArgFn<T>>) {
+        this.resolve(arg)
+        this.makePromise()
+    }
+
+    /** Throw an error for the next event. */
+    public deactivate(err: Error) {
+        this.reject(err)
+        this.makePromise()
+    }
+
+    /** Cancels the next event. */
+    public cancel() {
+        this.reject(Emitter.CANCEL_KEY)
+        this.makePromise()
+    }
+
+    /** Calls a function the next time is activated. */
+    public async once(fn: OneArgFn<T>) {
+        try {
+            fn(await this.next)
+        } catch (err) {
+            // Swallow Cancels
+            if (err !== Emitter.CANCEL_KEY)
+                throw err
+        }
+    }
+
+    /**
+     * Calls a function every time an event is activated.
+     * Stops after a cancellation.
+     */
+    public async on(fn: OneArgFn<T>) {
+        for await (const data of this.all)
+            fn(data)
+    }
+
+    /**
+     * Calls a function every time an event is activated
+     * Continues even after a cancellation.
+     */
+    public async every(fn: OneArgFn<T>, errFn?: OneArgFn<Error>) {
+        try {
+            for await (const data of this.all)
+                fn(data)
+        } catch (e) {
+            if (errFn)
+                errFn(e)
+            this.every(fn, errFn)
+        }
+    }
+
+    private makePromise() {
+        this.promises.push(new Promise((resolve, reject) => {
+            this.resolve = resolve
+            this.reject = reject
+        }))
+    }
+
+    private async* promiseGenerator(current = this.count): AsyncIterableIterator<T> {
+        try {
+            while (true)
+                yield this.promises[current++]
+        } catch (err) {
+            // Swallow Cancels
+            if (err !== Emitter.CANCEL_KEY)
+                throw err
+        }
+    }
+}
---
+++ test.ts
@@ -0,0 +1,213 @@
+import 'should'
+import Emitter from '.'
+
+describe('Classic EventEmitter', () => {
+
+    describe('Standard', () => {
+        const action = new Emitter
+
+        it('should be activated once', done => {
+            action.once(done)
+            action.activate()
+        })
+
+        it('should be activated three times', done => {
+            let times = 0
+            action.on(() => {
+                if (++times == 3)
+                    done()
+            })
+
+            action.activate()
+            action.activate()
+            action.activate()
+        })
+
+        it('should be activated more times', done => {
+            let times = 0
+            action.on(() => times++)
+            action.on(() => {
+                if(++times == 6)
+                    done()
+            })
+
+            action.activate()
+            action.activate()
+            action.activate()
+        })
+
+        it('should activate both once\'s', done => {
+            let times = 0
+            const hit = () => {
+                if(++times >= 2)
+                    done()
+            }
+            action.once(hit)
+            action.once(hit)
+
+            action.activate()
+        })
+    })
+
+    describe('With a value', () => {
+        const action = new Emitter<number>()
+
+        it('should be activated once with 12', done => {
+            action.once(val => {
+                if(val == 12)
+                    done()
+            })
+            action.activate(12)
+        })
+
+        it('should be activated three times increasing', done => {
+            let lastVal = 0
+            action.on(val => {
+                if (val == lastVal + 1)
+                    lastVal = val
+                if (lastVal == 3)
+                    done()
+            })
+
+            action.activate(1)
+            action.activate(2)
+            action.activate(3)
+        })
+    })
+
+    describe('Error Handling', () => {
+        const action = new Emitter
+
+        it('should cancel an event', done => {
+            let times = 0
+            action.on(() => {
+                if(++times >= 2)
+                    done()
+            })
+
+            action.activate()
+            action.activate()
+            action.cancel()
+            action.activate()
+        })
+
+        it.skip('should activate every\'s with an error', done => {
+            let times = 0
+            action.every(
+                () => {
+                    if (times++ == 100)
+                        done()
+                },
+                (err) => {
+                    err.should.be.an.instanceOf(Error)
+                    if(times == 3)
+                        times = 100
+                })
+
+            action.activate()
+            action.activate()
+            action.activate()
+            action.deactivate(Error('nothing'))
+            action.activate()
+        })
+    })
+})
+
+/**
+ * `setTimeout` is used to run a function after the listeners are put in place.
+ * In practice this would never need to be done since the thread activating the
+ * event is not the same as the one listening.
+ */
+describe('New Syntax EventEmitter', () => {
+
+    describe('Empty', () => {
+        const action = new Emitter
+
+        it('promise should resolve', async () => {
+            setTimeout(() => action.activate())
+            await action.next
+        })
+
+        it('should be activated three times', async () => {
+            setTimeout(() => {
+                action.activate()
+                action.activate()
+                action.activate()
+            })
+
+            let times = 0
+            for await (let _ of action.all)
+                if (++times == 3)
+                    break
+        })
+
+        it.skip('should be activated more times', async () => {
+            setTimeout(() => {
+                action.activate()
+                action.activate()
+                action.activate()
+                action.activate()
+            })
+
+            let times = 0
+            for await (let _ of action.all)
+                if (++times == 2)
+                    break
+            for await (let _ of action.all)
+                if (++times == 4)
+                    break
+            times.should.eql(4)
+        })
+    })
+
+    describe('With a value', () => {
+        const action = new Emitter<number>()
+
+        it('should be activated once with 12', async () => {
+            setTimeout(() => action.activate(12))
+            const val = await action.next
+            val.should.eql(12)
+        })
+    })
+
+    describe('Error Handling', () => {
+        const action = new Emitter
+
+        it('should cancel an event', async () => {
+            setTimeout(() => {
+                action.activate()
+                action.activate()
+                action.cancel()
+                action.activate()
+            })
+
+            let times = 0
+            for await (let _ of action.all)
+                times++
+            times.should.eql(2)
+        })
+
+        it('should activate many times, deactivate, then activate again', async () => {
+            setTimeout(() => {
+                action.activate()
+                action.activate()
+                action.activate()
+                action.deactivate(Error('nothing'))
+                action.activate()
+            })
+
+            let times = 0,
+                gotError = false
+            try {
+                for await (let _ of action.all)
+                    times++
+            } catch (e) {
+                gotError = true
+                e.message.should.eql('nothing')
+                await action.next
+            }
+            times.should.eql(3)
+            gotError.should.be.true()
+        })
+    })
+})
