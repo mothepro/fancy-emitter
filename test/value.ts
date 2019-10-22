3d50035ab580a38e5256b60077d7c2ddef20f84c
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2019-10-22 11:43:44 -0700

    Removed useless test

--- test/value.ts
+++ test/value.ts
@@ -1,45 +1,36 @@
 import { Emitter } from '../index'
 import later from './later'
 
-let actionNumber: Emitter<number>
+let action: Emitter<number>
 
 describe('Emitter with values', () => {
-    beforeEach(() => actionNumber = new Emitter)
+    beforeEach(() => action = new Emitter)
 
-    it('Classic, activated once with 12', done => {
-        actionNumber.once(val => {
+    it('activated once with 12', done => {
+        action.once(val => {
             if (val == 12)
                 done()
         })
-        actionNumber.activate(12)
+        action.activate(12)
     })
 
-    it('Fancy, activated once with 12', async () => {
-        later(() => actionNumber.activate(12))
-        const val = await actionNumber.next
+    it('activated once again with 12', async () => {
+        later(() => action.activate(12))
+        const val = await action.next
         val.should.eql(12)
     })
 
-    it('Classic, activated three times increasing', done => {
+    it('activated three times increasing', done => {
         let lastVal = 0
-        actionNumber.on(val => {
+        action.on(val => {
             if (val == lastVal + 1)
                 lastVal = val
             if (lastVal == 3)
                 done()
         })
 
-        actionNumber.activate(1)
+        action.activate(1)
             .activate(2)
             .activate(3)
-    })
-
-    xit('Fancy, Promise should not resolve unless listened to first', async () => {
-        later(() => actionNumber.activate(55))
-
-        actionNumber.activate(-100)
-
-        const num = await actionNumber.next
-        num.should.eql(55)
     })
 })
