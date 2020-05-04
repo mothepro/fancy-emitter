f23c7bf87d2d74babe8a409eb046af92e14ca24c
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-05-04 16:42:52 -0500

    Add the count member to the Listener interface

--- src/types.ts
+++ src/types.ts
@@ -7,24 +7,25 @@ export type OneArgFn<T, R = void> =
             : (arg?: T) => R // T is a combination of void and non void
 
 export interface SafeBroadcaster<T = void> {
-    activate: OneArgFn<T, this>
+  activate: OneArgFn<T, this>
 }
 
 export interface Broadcaster<T = void> extends SafeBroadcaster<T> {
-    deactivate(err: Error): this
-    cancel(): this
+  deactivate(err: Error): this
+  cancel(): this
 }
 
 export interface SafeListener<T = void> extends AsyncIterable<T> {
-    // Resolves when event is activated.
-    // Rejects when event is deactivated or cancelled.
-    readonly next: Promise<T>
+  // Resolves when event is activated.
+  // Rejects when event is deactivated or cancelled.
+  readonly next: Promise<T>
+  readonly count: number
 
-    once(fn: OneArgFn<T>): Promise<void>
-    on(fn: OneArgFn<T>): Promise<void>
+  once(fn: OneArgFn<T>): Promise<void>
+  on(fn: OneArgFn<T>): Promise<void>
 }
 
 export interface Listener<T = void> extends SafeListener<T> {
-    onceCancellable(fn: OneArgFn<T>, errFn?: (err: Error) => void): Function
-    onCancellable(fn: OneArgFn<T>, errFn?: (err: Error) => void): Function
+  onceCancellable(fn: OneArgFn<T>, errFn?: (err: Error) => void): Function
+  onCancellable(fn: OneArgFn<T>, errFn?: (err: Error) => void): Function
 }
