bd41bf762f4950c36ca2ff2633185fcc9beb9e7c
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-05-19 00:30:36 -0500

    Add better test for filtering

--- src/helpers/filter.ts
+++ src/helpers/filter.ts
@@ -1,15 +1,22 @@
 import type { SafeListener } from '../types.js'
 import { throwError } from '../Emitter.js'
 
-/** Resolves once the `emitter` activates with `value`. Rejects if the emitter deactivates */
+/**
+ * Resolves with `true` once the `emitter` activates with `value`.
+ * Rejects if the emitter deactivates
+ * Resolves with `false` if cancelled.
+ * 
+ * Usually the boolean returned can be ignored...
+ */
 export async function filter<T>(emitter: SafeListener<T>, condition: (arg: T) => boolean) {
   try {
     for await (const current of emitter)
       if (condition(current))
-        return
+        return true
   } catch (err) {
     throwError(err)
   }
+  return false
 }
 
 /** Resolves once the `emitter` activates with `value`. Rejects if the emitter deactivates */
--- test/filtered.ts
+++ test/filtered.ts
@@ -1,14 +1,43 @@
-import { filterValue, SafeEmitter } from '../index.js'
+import { filterValue, filter, SafeEmitter, Emitter } from '../index.js'
 
-it('Filter an emitter', async () => {
-  const action = new SafeEmitter<number>()
-  const positive = filterValue(action, 100)
-  const negative = filterValue(action, -10)
+describe('Filtered', () => {
+  it('Safe Emitter', () => {
+    const action = new SafeEmitter<number>()
+    const positive = filterValue(action, 100)
+    const negative = filterValue(action, -10)
 
-  action.activate(0)
-  action.activate(10)
-  action.activate(100)
+    action.activate(0)
+    action.activate(10)
+    action.activate(100)
 
-  negative.should.not.be.resolved()
-  positive.should.be.resolved()
+    negative.should.not.be.resolved()
+    positive.should.be.resolvedWith(true)
+  })
+
+  it('Conditional', () => {
+    const action = new Emitter<number>()
+    const promise = filter(action, num => num == action.count)
+
+    action.activate(0)
+
+    promise.should.be.resolved()
+  })
+
+  it('Deactivation', () => {
+    const action = new Emitter<number>()
+    const rejected = filterValue(action, 1)
+
+    action.deactivate(Error('deactivation'))
+
+    rejected.should.be.rejectedWith('deactivation')
+  })
+
+  it('Cancelled', () => {
+    const action = new Emitter<number>()
+    const cancelled = filterValue(action, 1)
+
+    action.cancel()
+
+    cancelled.should.be.resolvedWith(false)
+  })
 })
