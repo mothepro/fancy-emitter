de5d709b244f7eb309912eab03bac4bc380f76f1
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2020-06-12 02:06:32 -0500

    Add a helper to bind emitters to eventemitters

---
+++ src/helpers/bind.ts
@@ -0,0 +1,29 @@
+import SafeEmitter from '../SafeEmitter.js'
+import { Emitter } from '../../index.js'
+
+type Key = string | number | symbol
+
+interface ClassicEmitter {
+  addEventListener(type: Key, listener: (this: this, ev: Key) => any): void
+  addEventListener(type: Key, listener: (this: this, ev: Key) => any, ...options: any[]): void
+}
+
+export default function (emitter: ClassicEmitter, ...keys: Key[]) {
+  const safe = new SafeEmitter
+  for (const key of keys)
+    emitter.addEventListener(key, safe.activate)
+  return safe
+}
+
+export function bindOptions(emitter: ClassicEmitter, key: Key, ...options: any[]) {
+  const safe = new SafeEmitter
+  emitter.addEventListener(key, safe.activate, ...options)
+  return safe
+}
+
+export function bindCatchErrors(emitter: ClassicEmitter, key: Key, ...options: any[]) {
+  const unsafe = new Emitter
+  emitter.addEventListener(key, unsafe.activate, ...options)
+  emitter.addEventListener('error', unsafe.deactivate as any, ...options)
+  return unsafe
+}
---
+++ src/helpers/unbind.ts
@@ -0,0 +1,24 @@
+import { Emitter } from '../../index.js'
+import { SafeBroadcaster, Broadcaster } from '../types.js'
+
+type Key = string | number | symbol
+
+interface ClassicEmitter {
+  removeEventListener(type: Key, listener: (this: this, ev: Key) => any): void
+  removeEventListener(type: Key, listener: (this: this, ev: Key) => any, ...options: any[]): void
+}
+
+export default function (emitter: ClassicEmitter, broadcaster: SafeBroadcaster, ...keys: Key[]) {
+  for (const key of keys)
+    emitter.removeEventListener(key, broadcaster.activate)
+}
+
+export function unbindOptions(emitter: ClassicEmitter, broadcaster: SafeBroadcaster, key: Key, ...options: any[]) {
+  emitter.removeEventListener(key, broadcaster.activate, ...options)
+}
+
+export function unbindCatchErrors(emitter: ClassicEmitter, broadcaster: Broadcaster, key: Key, ...options: any[]) {
+  emitter.removeEventListener(key, broadcaster.activate, ...options)
+  emitter.removeEventListener('error', broadcaster.deactivate as any, ...options)
+}
+
